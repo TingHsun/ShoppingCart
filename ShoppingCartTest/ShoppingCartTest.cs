e9b3d70222c92a913b6cf74248b921aa75483898
Max <Max@MAX-PC>
2016-10-23 12:21:22 +0800

    買四本書第一個紅燈

--- ShoppingCartTest/ShoppingCartTest.cs
+++ ShoppingCartTest/ShoppingCartTest.cs
@@ -64,5 +64,26 @@ namespace ShoppingCartTest
             //Assert
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void buy_4_books_ep1_ep2_ep3_ep4_in_shoppingcart()
+        {
+            //Arrange
+            double expected = 320;
+
+            //Act
+            var books = new List<BookDao>
+            {
+                new BookDao { Episode = "1", Price = 100 },
+                new BookDao { Episode = "2", Price = 100 },
+                new BookDao { Episode = "3", Price = 100 },
+                new BookDao { Episode = "4", Price = 100 },
+            };
+            Basket basket = new Basket();
+            double actual = basket.CalculateThePrice(books);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
